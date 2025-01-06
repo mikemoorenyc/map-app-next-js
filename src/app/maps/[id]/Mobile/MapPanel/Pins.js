c3d5f0c607f3e49aa0ea5a31fe8ade121d267c66
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-01-06 12:50:24 -0500

    still trying to figure out optimal load zoom position

--- src/app/maps/[id]/Mobile/MapPanel/Pins.js
+++ src/app/maps/[id]/Mobile/MapPanel/Pins.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo } from "react";
+import { useContext, useEffect, useMemo,useState } from "react";
 import DataContext from "@/app/contexts/DataContext";
 import MobileActiveContext from "@/app/contexts/MobileActiveContext";
 import { useMap } from "@vis.gl/react-google-maps";
@@ -8,18 +8,25 @@ const Pins = () => {
   const map = useMap(); 
   const {disabledLayers, activePin} = useContext(MobileActiveContext).activeData 
   const {layerData} = useContext(DataContext);
+  const [firstLoad,updateFirstLoad] = useState(false);
   const pinsFlat = useMemo(()=>layerData.map(l => l.pins).flat(),[layerData])
   useEffect(()=> {
-    if(!map) return ;
+    if(!map || !pinsFlat.length) return ;
+    if(firstLoad) return ; 
+    
+    updateFirstLoad(true);
     var bounds = new google.maps.LatLngBounds();
     pinsFlat.forEach(p => {
        bounds.extend(p.location);
     })
-    map.fitBounds(bounds);
-    map.setZoom(12);
+    map.fitBounds(bounds, 1
+    );
+
+  
+    
 
 
-  },[map])
+  },[map,pinsFlat])
   return <>
   {pinsFlat.filter(p => !disabledLayers.includes(p.layerId)).map((pin)=><Marker active={activePin == pin.id} pin={pin}  key={pin.id}/>)}
 
