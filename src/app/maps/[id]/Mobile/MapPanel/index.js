f7e1b8be1dcd05389ad608cfbf8ad558b055bb4a
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-02-25 17:32:18 -0500

    updated fixes for lightmode on mobile

--- src/app/maps/[id]/Mobile/MapPanel/index.js
+++ src/app/maps/[id]/Mobile/MapPanel/index.js
@@ -1,5 +1,5 @@
 'use client'
-import { useContext } from "react"
+import { useContext ,useEffect,useState} from "react"
 import { APIProvider, Map } from "@vis.gl/react-google-maps"
 import Pins from "./Pins"
 import MobileSearch from "./MobileSearch"
@@ -10,7 +10,29 @@ import Updater from "../../desktop/TopMenu/Updater"
 import styles from "../"
 
 const MapPanel = () => {
+  const darkModeId = process.env.NEXT_PUBLIC_MAP_MOBILE_ID;
+  const lightModeId = process.env.NEXT_PUBLIC_MAP_EDITOR_ID;
   const {activeDispatch} = useContext(MobileActiveContext)
+  const [mapStyleId,updateMapStyleId] = useState(darkModeId);
+  useEffect(()=> {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    updateMapStyleId(darkModeId);
+    } else {
+      updateMapStyleId(lightModeId);
+    }
+    const changeMode = (event) => {
+      if(event.matches) {
+        updateMapStyleId(darkModeId);
+      } else {
+        updateMapStyleId(lightModeId);
+      }
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeMode);
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', changeMode);
+    }
+
+  },[])
   const closeActive = (e) => {
     activeDispatch({type: "SET_ACTIVE_PIN",id:null})
     activeDispatch({type: "DRAWER_STATE", state: "minimized"});
@@ -24,7 +46,7 @@ const MapPanel = () => {
   return <div className="mobile-app" style={{position:"absolute", width:"100%",height:"100vh"}}><APIProvider apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY}>
       <Map
       onClick={closeActive}
-      mapId={process.env.NEXT_PUBLIC_MAP_MOBILE_ID}
+      mapId={mapStyleId}
       defaultZoom={3}
       defaultCenter={{lat: 22.54992, lng: 0}}
       gestureHandling={'greedy'}
