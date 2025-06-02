2a1701b77aea39704cc23e156ba092492f32b2de
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-02 10:22:02 -0400

    Update updateMapGeneric.js

--- src/app/actions/updateMapGeneric.js
+++ src/app/actions/updateMapGeneric.js
@@ -1,1 +1,38 @@
+"use server";
+import { PutCommand,ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { ddbDocClient } from "../lib/dynamodb/ddbDocClient"
+import { GetCommand,UpdateCommand,DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
+
+
+const updateMap = async (mapId, payload={}) => {
+  const updateExpression = [];
+  let attributes = {
+    ":modified_at": new Date().toLocaleString()
+  }
+  for (const [key, value] of Object.entries(payload)) {
+    updateExpression.push(`${key}=:${key}`);
+    attributes[`:${key}`] = value
+  }
+  if(!updateExpression.length) return;
+
+  const command = {
+    TableName: "MapApp",
+    Key: {
+      id: mapId
+    },
+    UpdateExpression: `set ${updateExpression.join(",")}`,
+    ExpressionAttributeValues: attributes,
+    ReturnValues: "ALL_NEW"
+  }
+  try {
+    const updatedItem = await ddbDocClient.send(new UpdateCommand(command));
+    return updatedItem;
+  } catch(err) {
+    console.log(Error,"error"); 
+    return false;
+  }
+
+
+
+}
