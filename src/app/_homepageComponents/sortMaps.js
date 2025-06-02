922444d3145f40cf6dbc760dc6cf26f2f5316ba7
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-02 09:52:51 -0400

    made sort jsaction. parts can be repurposed other placess

--- src/app/_homepageComponents/sortMaps.js
+++ src/app/_homepageComponents/sortMaps.js
@@ -1,1 +1,29 @@
+const sortMaps = async (mapData,updateFunction, mapId,goingUp) => {
+  const {active,archived} = sortMaps(mapData);
+  const currentIndex = active.findIndex(m=>m.id === mapId);
+  if(!currentIndex) alert("couldn't find item"); return false; 
+  const newActives = reindexMap(active.splice(goingUp? currentIndex-1 : currentIndex+1 ,0,itemToMove));
+  const newMapData = [...newActives,...archived];
+  updateFunction(newMapData);
+  for(const map of newActives) {
+    const oldData = mapData.find(m=>m.id === map.id);
+    if(!oldData) continue; 
+    if(oldData.sortOrder === map.sortOrder) continue; 
 
+    const updatedItem = await updateMapServer(mapId,{
+      UpdateExpression: "set sortOrder = :sortOrder",
+      ExpressionAttributeValues: {
+        ":sortOrder": map.sortOrder
+      }
+    })
+    if(!updatedItem) alert("couldn't update Item on server"); 
+    updateFunction(newMapData.map(m => {
+      if(m.id === map.id) {
+        return updatedItem;
+      } else {
+        return m;
+      }
+      
+    })
+  }
+}
