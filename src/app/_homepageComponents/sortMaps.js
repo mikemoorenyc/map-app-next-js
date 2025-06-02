b1c90d3f0af390a09c219681342607b697087aad
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-02 11:18:09 -0400

    Update sortMaps.js

--- src/app/_homepageComponents/sortMaps.js
+++ src/app/_homepageComponents/sortMaps.js
@@ -5,22 +5,60 @@ const sortMaps = async (mapData,updateFunction, mapId,goingUp) => {
   const newActives = reindexMap(active.splice(goingUp? currentIndex-1 : currentIndex+1 ,0,itemToMove));
   const newMapData = [...newActives,...archived];
   updateFunction(newMapData);
-  for(const map of newActives) {
-    const oldData = mapData.find(m=>m.id === map.id);
-    if(!oldData) continue; 
-    if(oldData.sortOrder === map.sortOrder) continue; 
-
-    const updatedItem = await updateMapServer(mapId,{
-      sortOrder: map.sortOrder
-    })
-    if(!updatedItem) alert("couldn't update Item on server"); 
-    updateFunction(newMapData.map(m => {
-      if(m.id === map.id) {
-        return updatedItem;
-      } else {
-        return m;
-      }
-      
-    })
+  const updatedOnServer = await updateMaps(mapData,newMapData); 
+  if(!updatedOnServer) {
+    alert("couldn't update sort"); 
+    updateFunction(mapData); 
+    return false; 
   }
+  updateFunction(updatedOnServer); 
+}
+
+const updateMaps = async (oldMapData,newMapData, mapId, updatePackage) {
+  for (const map of newMapData) {
+    const oldData = oldMapData.find(m => m.id === map.id);
+    if(!oldData) continue; 
+    if(oldData.sortOrder === map.sortOrder && oldData.id !== mapId) continue; 
+    let payload = {
+      sortOrder: map.sortOrder
+    }
+    if(map.id === mapId) {
+      payload = {...payload,...updatePackage}
+    }
+    try {
+      const updatedItem = await updateMapServer(map.id,payload);
+      
+
+    } catch(err) {
+      console.log("ERROR",err)
+      return false; 
+    }
+
+
+  }
+  return getAllMaps(); 
+
+
+}
+const archiveMap = async (mapData,mapId, toArchive, updateFunction) => {
+  const toMove = {...mapData.find(m => m.id === mapId), ...{isArchived: toArchive}; 
+  const sorted = mapSort(mapData); 
+  const newActive = reindexMap(toArchive ? sorted.active.filter(m=>m.id !== mapId) ? [...sorted.active,...toMove]);
+  const newArchive = reindexMap(toArchive ? [...toMove,...sorted.archived] : sorted.archived.filter(m=>m.id !== mapId); 
+  const updatedMapData = [...newActive,...newArchive]; 
+  updateFunction(updatedMapData); 
+  const updatedOnServer = await updateMaps(updatedMapData); 
+  if(!updatedOnServer) alert("Couldn't updated on server"); updateFunction(mapData); return false; 
+  updateFunction(updatedOnServer); 
+}
+
+const deleteMap = async (mapData,mapId,updateFunction) => {
+  const deleted = mapSort(sortMaps.filter(m=>m.id !== mapId)); 
+  const reIndexed = [...reindexMap(deleted.active),...reindexMap(deleted.archived)];
+  updateFunction(reIndexed); 
+  const deletedMap = await deleteMap(mapId); 
+  if(!deleteMap) alert("Couldn't delete on server"); updateFunction(mapData); 
+  const updatedOnServer = await updateMaps(mapData,reIndexed);
+  if(!updatedOnServer) alert("Couldn't update"); updateFunction(mapData) return false; 
+  updateFunction(updatedOnServer);
 }
