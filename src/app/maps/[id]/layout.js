f0cf849e8b6a0799eee3ae82832c75bc5daf33fe
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-11 14:50:50 -0400

    Update layout.js

--- src/app/maps/[id]/layout.js
+++ src/app/maps/[id]/layout.js
@@ -7,7 +7,7 @@ export async function generateMetadata({params}) {
   const {id} = await params; 
  let mapData = await getMap(parseInt(id));
   return {
-    title: `${mapData.title} - Map App`,
+    title: `${mapData.mapIcon||""}${mapData.title} - Map App`,
     appleWebApp: {
     title: mapData.title
   }
