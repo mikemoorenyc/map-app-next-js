e78163620c1ce1d33f27d701e9883d7e9d6d8c57
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-12 11:35:13 -0400

    Update CustomColorPicker.jsx

--- src/app/components/AddMapForm/ColorPicker/CustomColorPicker.jsx
+++ src/app/components/AddMapForm/ColorPicker/CustomColorPicker.jsx
@@ -1,1 +1,36 @@
+import {useState} from "react"
+//import Button
+//import useKeyPress
 
+export default function CustomColorPicker(props) {
+  const {updateFunction,closeFunction,currentColor,isCustom} = props;
+  const [tempColor,updateTempColor] = useState(isCustom? currentColor : "#814444"); 
+  const save = () => {
+    updateFunction(tempColor);
+    closeFunction();
+  }
+  const cancel = () => {
+    closeFunction();
+  }
+  const saveOnEnter = useKeyPress("Enter",save);
+  
+  return (<>
+    <HexColorPicker color={tempColor} onChange={updateTempColor} />
+    <HexColorInput color={tempColor} onChange={updateTempColor} />
+    <div className={buttonsstyles}>
+    <Button
+      onClick={cancel}
+      modifiers={["sm","secondary"]}
+    >
+      Cancel
+    </Button>
+    <Button
+      onClick={save}
+      modifiers={["sm"]}
+    >
+      Save
+    </Button>
+    </div>
+    
+  </>)
+}
