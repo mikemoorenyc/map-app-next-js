a1c8b07c4fbcc288694145e28ab16b1070d38538
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-06-12 10:43:03 -0400

    Update ColorPicker.jsx

--- src/app/maps/[id]/sharedComponents/ColorPicker.jsx
+++ src/app/maps/[id]/sharedComponents/ColorPicker.jsx
@@ -1,1 +1,18 @@
+import {useState} from "react" 
 
+
+
+export default function ColorPicker ({initColor, saveColor}) {
+  const [color,updateColor] = useState(initColor); 
+
+  return <div>
+    <HexColorPicker color={color} onChange={setColor} />
+    <HexColorInput color={color} onChange={setColor} />
+    
+  </div>
+
+  
+
+
+
+}
