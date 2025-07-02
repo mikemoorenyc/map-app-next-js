c80fdb13d9669d627c19e02a175856f5f46e298f
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-07-02 10:51:20 -0400

    Create fieldMapping.js

---
+++ src/app/components/ModernSearch/lib/fieldMapping.js
@@ -0,0 +1,13 @@
+
+const mapping = [
+  ["id","id"],
+  ["displayName","name"],
+  ["formattedAddress":"formatted_address"],
+  ["websiteURI":"website"],
+  ["internationalPhoneNumber","international_phone_number"],
+  ["googleMapsURI","url"],
+  ["location","geometry"],
+  ["viewport","viewport"]
+]
+
+export default mapping
