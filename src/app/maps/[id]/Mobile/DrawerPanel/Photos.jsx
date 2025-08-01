e82297e54e397c9eeba77bf40b3bdd307e5dca56
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-08-01 13:07:15 -0400

    you now have to manually open the photos

--- src/app/maps/[id]/Mobile/DrawerPanel/Photos.jsx
+++ src/app/maps/[id]/Mobile/DrawerPanel/Photos.jsx
@@ -1,12 +1,14 @@
 import { useMapsLibrary} from "@vis.gl/react-google-maps"
-import { useCallback, useEffect,useState,useRef } from "react"
+import { useCallback, useEffect,useState,useRef, useContext } from "react"
 import styles from "./styles.module.css";
-
+import Button from "@/app/components/Button";
+import { RiCameraLine } from "@remixicon/react";
+import MobileActiveContext from "@/app/contexts/MobileActiveContext";
 
 export default function({id}) {
   const [photos,updatePhotos] = useState([]);
- 
-
+  const [photosShown,updatePhotosShown] = useState(false);
+  const {activeDispatch} = useContext(MobileActiveContext);
  
   const places = useMapsLibrary('places');
   const savedPhotos = JSON.parse(sessionStorage.getItem("saved_photos") || "[]");
@@ -31,6 +33,7 @@ export default function({id}) {
   useEffect(()=> {
     //CLEAR PHOTOS ON ID CHANGE
     updatePhotos([]);
+    updatePhotosShown(false);
     if(!id || typeof id !== "string" ) return ; 
     const saved = savedPhotos.find(p=>p.id==id);
     if(saved) {
@@ -39,9 +42,20 @@ export default function({id}) {
     }
     findPhotos(id) 
   },[id])
+
+  const showPhotos = () => {
+    updatePhotosShown(true);
+    activeDispatch({
+      type:"DRAWER_STATE",
+      state:"maximized"
+    })
+  }
  
-  
- 
+  if(photos.length < 1) return ;
+
+  if(!photosShown ) {
+    return <Button onClick={showPhotos} icon={<RiCameraLine />}>See photos</Button>
+  }
 
 
 return <div className={styles.containerFlex}>
