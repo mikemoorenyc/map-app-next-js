d1f360949b1d8d43cb958e92f0d756859df3e540
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-09-09 20:12:07 -0400

    remove contenttype that did nothing

--- src/app/api/glyph/route.tsx
+++ src/app/api/glyph/route.tsx
@@ -1,7 +1,7 @@
 
 import { ImageResponse } from 'next/og'
 import { NextRequest } from 'next/server';
-export const contentType = 'image/png'
+
 export async function GET(request:NextRequest,) {
 
  
