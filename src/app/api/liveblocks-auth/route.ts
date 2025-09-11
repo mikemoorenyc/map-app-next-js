679b070c74862b4f980bc52a827176f6e2346aa4
Mike Moore <6867676+mikemoorenyc@users.noreply.github.com>
2025-09-11 10:17:32 -0400

    Create route.ts

---
+++ src/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,34 @@
+import { Liveblocks } from "@liveblocks/node";
+import { auth } from "../auth"
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET,
+});
+
+export async function POST(request: Request) {
+
+  const authjsSession = await auth(); 
+  if(!authjsSession?.user) {
+    return new Response("Unauthorized",{status:401}); 
+  }
+  const {user} = authjsSession as TUSER; 
+  // Start an auth session inside your endpoint
+  const session = liveblocks.prepareSession(
+    user.email,
+    {
+      userInfo: {
+        name:user.name,
+        email:user.email
+      }
+    }// Optional
+  );
+
+  // Use a naming pattern to allow access to rooms with wildcards
+  // Giving the user read access on their org, and write access on their group
+  session.allow(`map-*`, session.READ_ACCESS);
+  session.allow(`map-:*`, session.FULL_ACCESS);
+
+  // Authorize the user and return the result
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+}
